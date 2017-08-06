897f26703fcb2cbe51280cea35e747e3df226a50
jeacevedo92 <jeacevedo92@utp.edu.co>
2017-08-06 00:07:18 -0500

    The program writes the result to a text file

--- Taller_1/mult_matrices.c
+++ Taller_1/mult_matrices.c
@@ -1,5 +1,9 @@
+// Programa que multiplica matrices las  cuales son leidas de dos archivos de texto.
+//realizado por: JHon Edinson Acevedo
+// agosto de 2017
+//Computacion de alto desempeño
+
 #include<stdlib.h>
-#include<time.h>
 #include<stdio.h>
 
 
@@ -68,7 +72,15 @@ void Write(FILE *Result_file, int** Result, int rows, int cols){
 	}
 }
 
-int main(){
+int main(int argc, char** argv){
+
+
+  if(argc != 3){
+    printf("Se necesitan como argumentos los nombres de los dos archivos de texto !!! \n");
+    return 1;
+	}
+
+
 
 	int filA,colA,filB,colB;
 
@@ -77,8 +89,10 @@ int main(){
 
 	FILE *Result_file;
 
-	matrizA = fopen("matrizA.txt","r");
-	matrizB = fopen("matrizB.txt","r");
+	//se abren los archivos de texto con los nombres recibidos por parametro
+
+	matrizA = fopen(argv[1],"r");
+	matrizB = fopen(argv[2],"r");
 	Result_file = fopen("matriz_resultado.txt","w");
 
 
@@ -88,6 +102,7 @@ int main(){
 	fscanf (matrizB,"%d",&filB);
 	fscanf (matrizB,"%d",&colB);
 
+	// se verifica que las dos matrices se puedan multiplicar !!!
 	if(colA!=filB){
 		printf(" No se pueden multiplicar las matrices, el numero de columnas de la primer matriz debe ser igual al numero de filas de la segunda matriz!!! \n");
 		return 1;
@@ -95,24 +110,27 @@ int main(){
 
 	int** A;int** B;int** Result;
 
-
+	//inicializan las matrices a utilizar 
 	A = init(A,filA,colA);
 	B = init(B,filB,colB);
 	Result = init(Result,filA,colB);
 
+	//se lee el archivo de texto y se guarda en la matriz los valores obtenidos.
 	llenaMatrices(matrizA,A,filA,colA);
 	printf(" Matriz A: \n");
-	imprimeMatrices(A,filA,colA);
-	
+	imprimeMatrices(A,filA,colA);	
 
 	llenaMatrices(matrizB,B,filB,colB);
 	printf(" Matriz B: \n");
 	imprimeMatrices(B,filB,colB);
 
+
+	//Se realiza la multiplicacion de las dos matrices
 	multiplicaMatrices(A,filA,colA,B,filB,colB,Result);
 	printf(" Multiplicación: \n");
 	imprimeMatrices(Result,filA,colB);
 
+	//se escribe en u archivo de texto la matriz resultante
 	Write(Result_file,Result,filA,colB);	
 
 	free(*A);free(*B);free(*Result);//liberamos memoria
